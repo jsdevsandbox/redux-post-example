a534066683033cf46f1eb8dd2ac9d5dca8234d3d
jsdevsandbox <jsdevsandbox@gmail.com>
2019-04-22 23:43:15 -0700

    adding integration tests to test the redux store state updates

--- src/integration.test.js
+++ src/integration.test.js
@@ -12,12 +12,42 @@ describe("Integration Tests for Redux Store", () => {
   });
 
   it("should update state in store", () => {
-    const expectedResponse = [{ title: "title1", body: "body text 1" }];
-    fetchMock.mock(
-      "https://jsonplaceholder.typicode.com/post",
-      expectedResponse
-    );
+    // fetchPosts()
+    let expectedResponse = {
+      posts: {
+        item: {},
+        items: { body: "this is the body of Post 1", id: "1", title: "Post 1" }
+      }
+    };
 
-    // need to figure this out later
+    store.dispatch({
+      type: FETCH_POSTS,
+      payload: {
+        title: "Post 1",
+        id: "1",
+        body: "this is the body of Post 1"
+      }
+    });
+
+    expect(store.getState()).toEqual(expectedResponse);
+
+    // createPost()
+    expectedResponse = {
+      posts: {
+        item: { body: "this is the body of Post 2", id: "2", title: "Post 2" },
+        items: { body: "this is the body of Post 1", id: "1", title: "Post 1" }
+      }
+    };
+
+    store.dispatch({
+      type: NEW_POST,
+      payload: {
+        title: "Post 2",
+        id: "2",
+        body: "this is the body of Post 2"
+      }
+    });
+
+    expect(store.getState()).toEqual(expectedResponse);
   });
 });
